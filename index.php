af5703ced6c88a0bbd2fb360fe032eb8627ff460
Spymannn <samir_hanini@live.be>
2014-09-29 14:23:58 +0200

    2 commit

--- index.php
+++ index.php
@@ -4,8 +4,16 @@
         <title> Site 1</title>
     </head>
     <body>
-        <?php
         
+         <?php 
+            $connexion = "host=localhost password=benwallace03 user=samir.hanini dbname=samir_hanini_xml port=5432";
+            $cnx = pg_connect($connexion);
+            if($cnx){
+                print("ok");
+            }
+            else{
+                print("Echec");
+            }
         ?>
         
     </body>
