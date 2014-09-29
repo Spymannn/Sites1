0d19c7c72f3a0a58272e032a765b11480b2138cf
Spymannn <samir_hanini@live.be>
2014-09-29 13:57:00 +0200

    Premier commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title> Site 1</title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+        
+    </body>
+    
+    
+</html>
