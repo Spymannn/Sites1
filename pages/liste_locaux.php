9397f917ef645618614da9fb44c98ef718b93443
Spymannn <samir_hanini@live.be>
2014-09-29 14:29:44 +0200

    3eme commit

---
+++ pages/liste_locaux.php
@@ -0,0 +1,11 @@
+<h2> Liste des locaux de Mons </h2>
+
+<section>
+    <h3> Batiment F </h3>
+    <ul>
+        <li> Labo 1 </li>
+        <li> Labo 2 </li>
+        <li> Labo 3 </li>
+    </ul>
+    
+</section>
